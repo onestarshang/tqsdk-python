76ceb17e392fb6ece7f4cfbbfaeebd17cd73d88c
shinny-mayanqiong <mayanqiong@shinnytech.com>
2020-05-21 20:02:41 +0800

    utils: 添加日志相关函数

--- tqsdk/utils.py
+++ tqsdk/utils.py
@@ -2,15 +2,54 @@
 #-*- coding:utf-8 -*-
 __author__ = 'yanqiong'
 
+import datetime
+import logging
+import os
 import random
 import uuid
-
+from functools import wraps
 
 RD = random.Random()  # 初始化随机数引擎
+DEBUG_DIR = os.path.join(os.path.expanduser('~'), ".tqsdk/logs")
 
 
 def _generate_uuid(prefix=''):
     return f"{prefix + '_' if prefix else ''}{uuid.UUID(int=RD.getrandbits(128)).hex}"
+
+
+def _get_log_format(is_backtest=None):
+    """返回日志格式"""
+    if is_backtest:
+        return logging.Formatter('%(levelname)6s - %(message)s')
+    else:
+        return logging.Formatter('%(asctime)s - %(levelname)6s - %(message)s')
+
+
+def _get_log_name():
+    """返回默认 debug 文件生成的位置"""
+    if not os.path.exists(DEBUG_DIR):
+        os.makedirs(os.path.join(os.path.expanduser('~'), ".tqsdk/logs"), exist_ok=True)
+    return os.path.join(DEBUG_DIR, f"{datetime.datetime.now().strftime('%Y%m%d%H%M%S%f')}-{os.getpid()}.log")
+
+
+def _clear_logs():
+    """清除最后修改时间是 n 天前的日志"""
+    if not os.path.exists(DEBUG_DIR):
+        return
+    n = os.getenv("TQ_SAVE_LOG_DAYS", 30)
+    dt = datetime.datetime.now() - datetime.timedelta(days=int(n))
+    for log in os.listdir(DEBUG_DIR):
+        path = os.path.join(DEBUG_DIR, log)
+        if datetime.datetime.fromtimestamp(os.stat(path).st_mtime) < dt:
+            os.remove(path)
+
+
+def _log_decorator(f):
+    @wraps(f)
+    def wrapper(*args, **kwds):
+        logging.getLogger("TqApi").debug(f'Calling [ {f.__name__}() ] with {args} {kwds}')
+        return f(*args, **kwds)
+    return wrapper
 
 
 night_trading_table = {
